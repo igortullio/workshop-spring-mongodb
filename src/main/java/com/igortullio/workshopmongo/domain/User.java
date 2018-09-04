1189c59a1231dcb71d5d48366d6b49b97719cda9
Igor Túllio <igor.goncalves@basis.com.br>
2018-09-04 11:33:29 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/igortullio/workshopmongo/domain/User.java
@@ -0,0 +1,17 @@
+package com.igortullio.workshopmongo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+}
