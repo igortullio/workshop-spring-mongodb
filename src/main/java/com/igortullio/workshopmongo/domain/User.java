f98a8818ebefcd5dbe18379c9ae968cdfc8b6403
Igor Túllio <igor.goncalves@basis.com.br>
2018-09-04 15:46:37 -0300

    Referenciando os posts do usuário

--- src/main/java/com/igortullio/workshopmongo/domain/User.java
+++ src/main/java/com/igortullio/workshopmongo/domain/User.java
@@ -1,18 +1,26 @@
 package com.igortullio.workshopmongo.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
-@AllArgsConstructor
 @Document(collection = "user")
 public class User {
     @Id
     private String id;
     private String name;
     private String email;
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
