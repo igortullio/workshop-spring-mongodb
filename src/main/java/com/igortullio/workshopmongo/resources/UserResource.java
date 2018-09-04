2cd4c3d70151077274f340f07ffd95cd5458c6bc
Igor Túllio <igor.goncalves@basis.com.br>
2018-09-04 14:11:44 -0300

    Atualização de usuário com PUT

--- src/main/java/com/igortullio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/igortullio/workshopmongo/resources/UserResource.java
@@ -44,4 +44,12 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+        User user = UserService.fromDTO(userDTO);
+        user.setId(id);
+        userService.update(user);
+        return ResponseEntity.noContent().build();
+    }
+
 }
